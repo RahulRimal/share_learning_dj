f2182e09fc544d389f3fb34ef0b0c377f30ee346
Krishna Rimal <genuinetechnology9@gmail.com>
2022-09-19 15:09:37 +0545

    Added customer info change permission to group and admin or read only

--- share_learning/store/permissions.py
+++ share_learning/store/permissions.py
@@ -33,6 +33,8 @@ class IsSuperUser(permissions.BasePermission):
 
 class IsSuperOrHasCustomerPermissions(permissions.BasePermission):
     def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
         return bool(request.user.is_superuser or request.user.groups.filter(name='customer_modirator'))
 
 
